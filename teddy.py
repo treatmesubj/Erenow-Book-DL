5ac3e5af1a9b67f1cfe21694aeb4e2e236958e5e
John Hupperts <39680353+treatmesubj@users.noreply.github.com>
2020-12-14 13:41:01 -0600

    write index file

--- teddy.py
+++ teddy.py
@@ -11,14 +11,15 @@ if __name__ == "__main__":
 	except IndexError:
 		book_url = input("Book-URL: ")
 
-	soup = BeautifulSoup(requests.get(book_url).text, 'html.parser')
-	book_title = "".join(x for x in soup.select_one("h1").text if x.isalnum() or x in(" ", "-"))
+	main_soup = BeautifulSoup(requests.get(book_url).text, 'html.parser')
+	book_title = "".join(x for x in main_soup.select_one("h1").text if x.isalnum() or x in(" ", "-"))
 	print(f"{book_title=}")
 	book_path = f"{os.getcwd()}/{book_title}"
 	if not os.path.exists(book_path):
 		os.makedirs(book_path)
 
-	chapter_elems = soup.select("#text-center p a[href]")
+	# write chapters
+	chapter_elems = main_soup.select("#text-center p a[href]")
 	for chapter_elem in chapter_elems:
 
 		chapter_name = "".join(x for x in chapter_elem.text if x.isalnum() or x in(" ", "-"))
@@ -51,3 +52,10 @@ if __name__ == "__main__":
 		print(f"writing {chapter_name}...")
 		with open(f"{book_path}/{chapter_name}.html", "w+", encoding="utf8") as f:
 			f.write(str(soup))
+
+	# write index file
+	idx_soup = main_soup.select_one('#text-center')
+	for link in idx_soup.select("a[href]"):
+		link.attrs = {}
+	with open(f"{book_path}/Index_Info.html", "w+", encoding="utf8") as f:
+		f.write(str(idx_soup))
